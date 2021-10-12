da6fd0c4d86d0e35602a0cb9e327cb68867825f7
richard sandblom <r.sandblom@gmail.com>
2021-10-12 11:59:57 +0200

    Added point 4 in assigment instruction

---
+++ LexiconTodoIT.Tests/TodoTests.cs
@@ -0,0 +1,23 @@
+﻿using LexiconTodoIT.Model;
+using System;
+using Xunit;
+
+namespace LexiconTodoIT.Tests
+{
+    public class TodoTests
+    {
+        [Fact]
+        public void CreateWithIdAndDescription()
+        {
+            int id = 1;
+            string description = "TheDescription";
+
+            Todo todoToTest = new Todo(id, description);
+
+            Assert.NotNull(todoToTest);
+            Assert.Equal(id, todoToTest.TodoId);
+            Assert.Equal(description, todoToTest.Description);
+
+        }
+    }
+}
---
+++ LexiconTodoIT/Model/Todo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LexiconTodoIT.Model
+{
+    public class Todo
+    {
+        private readonly int todoId;
+        private string description;
+        private bool done;
+        private Person assignee;
+
+        public int TodoId { get { return todoId; } }
+
+        public string Description { get { return description; } set { description = value; } }
+
+        public Todo(int todoId, string description)
+        {
+            this.todoId = todoId;
+            this.description = description;
+        }
+    }
+}
