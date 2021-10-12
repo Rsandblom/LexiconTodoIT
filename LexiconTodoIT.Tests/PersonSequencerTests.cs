d021d3ade761e3f4b5f1ec4cc4ee07b6d50f9c3f
richard sandblom <r.sandblom@gmail.com>
2021-10-12 12:23:05 +0200

    Added points 5 and 6 in assigment instruction

---
+++ LexiconTodoIT.Tests/PersonSequencerTests.cs
@@ -0,0 +1,36 @@
+﻿using LexiconTodoIT.Data;
+using System;
+using Xunit;
+
+namespace LexiconTodoIT.Tests
+{
+    public class PersonSequencerTests
+    {
+        [Fact]
+        public void CallingNextPersonIdMethodShouldReturnIncrementedPersonId()
+        {
+            PersonSequencer.reset(); 
+            int idAfterIncrementMethodCall = 1;
+            int idAfterSecondIncrementMethodCall = 2;
+
+            Assert.Equal(idAfterIncrementMethodCall, PersonSequencer.nextPersonId() );
+            Assert.Equal(idAfterSecondIncrementMethodCall, PersonSequencer.nextPersonId());
+
+        }
+
+        [Fact]
+        public void CallingResetMethodShouldReturnZeroPersonId()
+        {
+            PersonSequencer.reset();
+            int idAfterIncrementMethodCall = 1;
+            int idAfterResetMethodCall = 0;
+
+            Assert.Equal(idAfterIncrementMethodCall, PersonSequencer.nextPersonId());
+
+            PersonSequencer.reset();
+
+            Assert.Equal(idAfterResetMethodCall, PersonSequencer.PersonId);
+
+        }
+    }
+}
---
+++ LexiconTodoIT/Data/PersonSequencer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LexiconTodoIT.Data
+{
+    public class PersonSequencer
+    {
+        private static int personId;
+
+        public static int PersonId { get {return personId; } }
+
+        public static int nextPersonId()
+        {
+            personId++;
+            return PersonId;
+        }
+
+        public static void reset()
+        {
+            personId = 0;
+        }
+
+    }
+}
