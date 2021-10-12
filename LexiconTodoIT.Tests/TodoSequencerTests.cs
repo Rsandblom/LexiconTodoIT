389c237ecd59379813d559d76df9b8c5428eb164
richard sandblom <r.sandblom@gmail.com>
2021-10-12 12:34:26 +0200

    Added points 7 in assigment instruction

---
+++ LexiconTodoIT.Tests/TodoSequencerTests.cs
@@ -0,0 +1,37 @@
+﻿using LexiconTodoIT.Data;
+using System;
+using Xunit;
+
+namespace LexiconTodoIT.Tests
+{
+    public class TodoSequencerTests
+    {
+        [Fact]
+        public void CallingNextTodoIdMethodShouldReturnIncrementedTodoId()
+        {
+            TodoSequencer.reset();
+            int idAfterIncrementMethodCall = 1;
+            int idAfterSecondIncrementMethodCall = 2;
+
+            Assert.Equal(idAfterIncrementMethodCall, TodoSequencer.nextTodoId());
+            Assert.Equal(idAfterSecondIncrementMethodCall, TodoSequencer.nextTodoId());
+
+        }
+
+        [Fact]
+        public void CallingResetMethodShouldReturnZeroTodoId()
+        {
+            TodoSequencer.reset();
+            int idAfterIncrementMethodCall = 1;
+            int idAfterResetMethodCall = 0;
+
+            Assert.Equal(idAfterIncrementMethodCall, TodoSequencer.nextTodoId());
+
+            TodoSequencer.reset();
+
+            Assert.Equal(idAfterResetMethodCall, TodoSequencer.TodoId);
+
+        }
+
+    }
+}
---
+++ LexiconTodoIT/Data/TodoSequencer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LexiconTodoIT.Data
+{
+    public class TodoSequencer
+    {
+        private static int todoId;
+
+        public static int TodoId { get { return todoId; } }
+
+        public static int nextTodoId()
+        {
+            todoId++;
+            return TodoId;
+        }
+
+        public static void reset()
+        {
+            todoId = 0;
+        }
+    }
+}
